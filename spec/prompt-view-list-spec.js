3230bf03bdbf0848b0baafa9f25a74ba0c5774ea
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-08-27 14:55:07 +0900

    Implement onConfirmed in PromptViewList
      ref #37

--- spec/prompt-view-list-spec.js
+++ spec/prompt-view-list-spec.js
@@ -12,4 +12,21 @@ describe('PromptViewList', () => {
       expect(list.getItem()).toBe('1')
     })
   })
+  describe('.onConfirmed', () => {
+    it('emit the event.', () => {
+      let isCalled = false
+      const callback = (item) => {
+        expect(item).toBe('1')
+        isCalled = true
+      }
+
+      const list = new PromptViewList()
+      list.initialize()
+      list.open(['1', '2', '3'])
+      list.onConfirmed(callback)
+      list.setSelectedItem('1')
+
+      expect(isCalled).toBe(true)
+    })
+  })
 })
