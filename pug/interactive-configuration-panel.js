6d289bcbab3cfc640887aa1b09b6b025fc03c73c
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-19 22:44:46 +0900

    Change pug files into js files to use require
      ref #65

--- pug/interactive-configuration-panel.js
+++ pug/interactive-configuration-panel.js
@@ -1,5 +1,9 @@
+'use babel'
+
+export default `
 div(class="section-container user-support-helper")
   div.container
     div.row
       span(class="text-right close-icon", href="#")
     div(class="row content")
+`
--- pug/prompt-view-dropdown.js
+++ pug/prompt-view-dropdown.js
@@ -1,3 +1,6 @@
-select.dropdown
+'use babel'
+
+export default `select.dropdown
   each choice in choices
     option(value=choice)=choice
+`
